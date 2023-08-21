d86db0b1d61e2e271d28c03d500f4c268aab5065
axtr <chau.alex88@hotmail.com>
2023-08-21 13:25:01 +0100

    Completed Day 2 with map

---
+++ Day2/part2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	input, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	sc := bufio.NewScanner(input)
+
+	var score int
+
+	// A : Rock
+	// B : Paper
+	// C : Scissors
+
+    // X : Lose
+    // Y : Draw
+    // Z : Win
+
+	scores := map[string]int{"B X": 1, "C X": 2, "A X": 3, "A Y": 4, "B Y": 5, "C Y": 6, "C Z": 7, "A Z": 8, "B Z": 9}
+
+	for sc.Scan() {
+		score += scores[sc.Text()]
+	}
+	fmt.Println(score)
+}
